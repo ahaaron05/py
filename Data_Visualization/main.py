bfde1e14f3fc9cc8062c7930173c6a9be594c263
Aaron Ludwig <107589333+ahaaron05@users.noreply.github.com>
2024-03-17 03:34:01 -0500

    started data visualization

---
+++ Data_Visualization/main.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+input_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+
+plt.style.use('dark_background')
+fig, ax = plt.subplots()
+print(plt.style.available)
+ax.plot(input_values, squares, linewidth=4)
+
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of value", fontsize=14)
+
+ax.tick_params(axis="both", labelsize=14)
+
+plt.show()
